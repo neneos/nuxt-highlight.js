c835f7aba4acd47613dda1eeb5c387538c558d3c
Robin <robin.soenen@gmail.com>
2018-07-25 19:49:24 +0200

    version 0.0.5

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const path = require('path')
 
-module.exports = function AnimateCss(moduleOptions) {
+module.exports = function highlightJs(moduleOptions) {
     this.addPlugin(path.resolve(__dirname, 'vue-highlight.js'))
+    this.options.css.push({ src: 'highlight.js/styles/default.css', lang: 'css' })
 }
 
 module.exports.meta = require('./package.json');
